a584a185e07ca6a48055228f391ebd3d38cfe841
edgarlorsch10 <elorschdev@gmail.com>
2026-03-10 16:34:57 +0300

    I added an index to the business_email column in this migration, since the login process felt a bit slow yet there were very few users who had registered on the platform, the essence of doing this was to speed up the process it takes to identify a client during the login process via email.

---
+++ database/migrations/2026_03_10_101530_add_uique_index_to_business_email.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('registered_users', function (Blueprint $table) {
+            $table->unique("business_email");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void{
+        Schema::table('business_email', function (Blueprint $table) {
+        $table->dropUnique(["business_email"]);
+        });
+    }
+};
